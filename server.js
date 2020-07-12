c0b7d8f217527f989f59d41ef22de83e5dc2775c
Justin Nguyen <nuggetnchill@gmail.com>
2020-07-12 14:46:51 -0500

    connection updated

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
   client: "pg",
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+    }
 });
 
 db.select('*').from('users').then(data =>{
